7b132d59f1dd869a16a0b8ca03d26e84669b3cc6
Eirik <eirikalv@icloud.com>
2024-08-27 17:28:39 +0200

    update

--- app/bot.py
+++ app/bot.py
@@ -12,6 +12,19 @@ intents.members = True
 intents.message_content = True
 client = Client(intents=intents)
 
+week = datetime.datetime.now().isocalendar()[1]
+date = datetime.datetime.now().strftime('%d/%m/%y')
+question = f"Uke {week}. Reager på hvilken tier dagens Biltema middag var! ({date})"
+poll = Poll(question, datetime.timedelta(hours=24))
+active_poll = False
+
+poll.add_answer(text="Baaaaanging!", emoji='🥇')
+poll.add_answer(text="Legendarisk", emoji='🟠')
+poll.add_answer(text="Utrolig", emoji='🟣')
+poll.add_answer(text="Veldig god", emoji='🔵')
+poll.add_answer(text="God", emoji='🟢')
+poll.add_answer(text="Clas Ohlson", emoji='🟤')
+
 users = {}
 with open('app/users.json', 'r') as file:
     users = json.load(file)
@@ -52,26 +65,23 @@ def user_is_guru(user):
             return True
     return False
 
-async def poll(message):
-    week = datetime.datetime.now().isocalendar()[1] - 26
-    date = datetime.datetime.now().strftime('%d/%m/%y')
-    question = f"Uke {week}. Reager på hvilken tier dagens Biltema middag var! ({date})"
-
-    poll = Poll(question, datetime.timedelta(hours=24))
-    poll.add_answer("w")
-    
-    await message.channel.send(poll)
-
-
 async def cmds(message):
     if message.content == '!poll':
-        await poll(message)
+        global active_poll, poll
+        active_poll = True
+        await message.channel.send("", poll=poll)
+
+        if poll.is_finalized() or True:
+            active_poll = False
+        
+        async for i in poll.answers[0].voters():
+            print(i)
 
     if len(message.content.split()) == 2:
         cmd, name = message.content.split()
         
         if not name in users:
-            users[name] = 0
+            users[name] = {'xp': 0, 'mote': 0}
         
         if cmd == "!xp":
             return f"{name} har {users[name]}xp"
@@ -87,21 +97,35 @@ async def cmds(message):
             print(f"{name} is not a user in the discord")
             return
         if not name in users:
-            users[name] = 0
+            users[name] = {'xp': 0, 'mote': 0}
 
         if cmd == "+xp":
-            users[name] += int(amount)
+            users[name]['xp'] += int(amount)
         
             with open('app/users.json', 'w') as file:
                 json.dump(users, file)
 
             return f"{name} fikk {amount}xp"
         if cmd == "-xp":
-            users[name] -= int(amount)
+            users[name]['xp'] -= int(amount)
 
             with open('app/users.json', 'w') as file:
                 json.dump(users, file)
 
             return f"{name} mistet {amount}xp"
+        if cmd == '+oppmøte':
+            users[name]['mote'] += int(amount)
+
+            with open('app/users.json', 'w') as file:
+                json.dump(users, file)
+
+            return f"{name} fikk {amount} oppmøte"
+        if cmd == '-oppmøte':
+            users[name]['mote'] -= int(amount)
+
+            with open('app/users.json', 'w') as file:
+                json.dump(users, file)
+
+            return f"{name} mistet {amount} oppmøte"
 
 client.run(token=token)
